10de7ba1a8ba7bd685645df5d58a62161e6944ce
yangzhou <yangzhou@yunjiblobal.com>
2020-02-28 17:34:23 +0800

    StackNavigation提交

---
+++ navigation/LoginPage.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Alert, TextInput} from 'react-native';
+import {NavigationActions, StackActions} from 'react-navigation';
+
+
+export default class LoginPage extends React.Component {
+
+    static navigationOptions = {
+        //header: null,
+        headerTitle: <TextInput placeholder={'请输入搜索内容-测试标题'}/>,
+        headerStyle: {backgroundColor: '#0086f1'},
+    };
+
+    componentWillMount() {
+        // this.props.navigation
+        //this.props.navigation.dispatch();
+
+        //this._closeBackView();
+    }
+
+    componentDidMount() {
+
+
+    }
+
+
+
+
+    render() {
+
+
+        return (
+
+            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+
+                <TouchableOpacity onPress={() => {
+
+                }}>
+                    <View style={{alignItems: 'center'}}>
+                        <Text style={{color: 'red', fontSize: 34}}>模拟登陆页面</Text>
+                        <Text style={{color: 'red', fontSize: 30}}>点击</Text>
+                    </View>
+                </TouchableOpacity>
+
+
+                <TouchableOpacity onPress={() => {
+                    this.props.navigation.goBack();
+                    //this._closeBackView();
+                }}>
+                    <View>
+                        <Text style={{fontSize: 24, marginTop: 20}}>返回上一页面</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        )
+            ;
+    }
+
+
+}
--- navigation/StackNavigation.js
+++ navigation/StackNavigation.js
@@ -4,24 +4,32 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import OnePage from './OnePage';
 import HomePage from './HomePage';
+import LoginPage from './LoginPage';
 import TowPage from './TowPage';
 import ThreePage from './ThreePage';
 import FourPage from './FourPage';
+
 
 const AppStackNav = createStackNavigator({
     Home: {
         screen: HomePage,
     },
-    OnePage: {
-        screen: OnePage,
-    }, TowPage: {
-        screen: TowPage,
+    Login: {
+        screen: LoginPage,
     },
-    ThreePage: {
-        screen: ThreePage,
-    }, FourPage: {
-        screen: FourPage,
-    },
+    // OnePage: {
+    //     screen: OnePage,
+    // }, TowPage: {
+    //     screen: TowPage,
+    // },
+    // ThreePage: {
+    //     screen: ThreePage,
+    // }, FourPage: {
+    //     screen: FourPage,
+    // },
+}, {
+    initialRouteName: 'Home',
+    headerMode: 'screen',
 });
 
 export default createAppContainer(AppStackNav);

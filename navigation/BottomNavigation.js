5b2d9f8af807d94101b9b505a76f3599cf6bb2bc
yangzhou <yangzhou@yunjiglobal.com>
2020-03-01 15:38:36 +0800

    增加，BottomNavigation效果

--- navigation/BottomNavigation.js
+++ navigation/BottomNavigation.js
@@ -1,30 +1,103 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+//import LogoNodejs from 'react-ionicons/lib/LogoNodejs'
+
 import OnePage from './OnePage';
 import TowPage from './TowPage';
 import ThreePage from './ThreePage';
 import FourPage from './FourPage';
 
-const BottomTabNavigation = createMaterialBottomTabNavigator({
+
+const BottomTabNavigation = createBottomTabNavigator({
     OnePage: {
         screen: OnePage,
+        navigationOptions: {
+            headerTitle: '首页',
+            gestureResponseDistance: {horizontal: 300},
+            headerBackTitle: null,
+            headerStyle: {backgroundColor: '#EB3695'},//导航栏的样式
+            headerTitleStyle: {//导航栏文字的样式
+                color: 'white',
+                //设置标题的大小
+                fontSize: 16,
+                //居中显示
+                alignSelf: 'center',
+            },
+            tabBarLabel: '主页',
+            tabBarIcon: ({focused,tintColor}) => {
+                let img = focused ? '.././images/tab_main_check_vip.png' : '.././images/tab_main_nocheck_vip.png';
+               return (<Image
+                   source={require('.././images/tab_main_nocheck_vip.png')}
+                   style={[{height: 24, width: 24}, {tintColor: tintColor}]}/>);
+            },
+        }
     },
     TowPage: {
         screen: TowPage,
+        navigationOptions: {
+            tabBarLabel: '活动',
+            tabBarIcon: ({tintColor}) => (
+                <Image
+                    source={require('.././images/tab_found_check_vip.png')}
+                    style={[{height: 24, width: 24}, {tintColor: tintColor}]}/>
+            ),
+        }
     },
     ThreePage: {
         screen: ThreePage,
+        navigationOptions: {
+            tabBarLabel: '购物车',
+            tabBarIcon: ({tintColor}) => (
+                <Image
+                    source={require('.././images/tab_cart_check_vip.png')}
+                    style={[{height: 24, width: 24}, {tintColor: tintColor}]}/>
+            ),
+        }
     },
     FourPage: {
         screen: FourPage,
+        navigationOptions: {
+            tabBarLabel: '我的',
+            tabBarIcon: ({tintColor}) => (
+                <Image
+                    source={require('.././images/tab_mine_check_vip.png')}
+                    style={[{height: 24, width: 24}, {tintColor: tintColor}]}/>
+            ),
+        }
     },
 }, {
     initialRouteName: 'OnePage',
-    activeColor: '#f0edf6',
-    inactiveColor: '#3e2465',
-    barStyle: {backgroundColor: '#694fad'},
+    //是否在更改标签时显示动画
+    // animationEnabled: true,
+    //是否允许在标签之间进行滑动
+    // swipeEnabled: true,
+    //tab间的类fragment配置
+    backBehavior:'none',
+    tabBarOptions: {
+        //共有属性
+        showIcon: true,//是否显示图标，默认开启
+        showLabel: true,//是否显示label，默认开启
+        activeTintColor: '#EB3695',//label和icon的前景色 活跃状态下（选中）
+        inactiveTintColor: 'gray',//label和icon的前景色 活跃状态下（未选中）
+        style: { //TabNavigator 的背景颜色
+            backgroundColor: 'white',
+            height: 55,
+        },
+
+        labelStyle: {//文字的样式
+            fontSize: 13,
+            marginTop: -5,
+            marginBottom: 5,
+        },
+        iconStyle: {//图标的样式
+            marginBottom: 5,
+        }
+    },
 });
 
 const AppContainer = createAppContainer(BottomTabNavigation);
--- navigation/OnePage.js
+++ navigation/OnePage.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Dimensions, TouchableOpacity} from 'react-native';
+
+
+let deviceWidth = Dimensions.get('window').width
+
 
 export default class HomePage extends React.Component {
 
-        // static navigationOptions = {
-        //     header: null,
-        // }
+    // static navigationOptions = {
+    //     header: null,
+    // }
+
+    _titleBar() {
+        return (<View style={{
+            height: 44,
+            width: deviceWidth,
+            backgroundColor: 'white',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <Text style={{fontSize: 20}}>首页面</Text>
+        </View>)
+    }
 
     render() {
         return (
-            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-                <Text style={{color: 'red', fontSize: 34}}>第一个页面</Text>
+
+            <View style={{flex: 1, flexDirection: 'column'}}>
+
+                {this._titleBar()}
+
+                <TouchableOpacity onPress={() => {
+
+                }}>
+                    <View>
+                        <Text
+                            style={{
+                                marginTop: 20,
+                                marginLeft: 40,
+                                padding: 10,
+                                backgroundColor: '#ffc033',
+                                fontSize: 18,
+                                marginRight: 40,
+                            }}>二级Tab页面</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                    <Text style={{color: 'red', fontSize: 34}}>模拟首页面-数据</Text>
+                </View>
             </View>
         );
     }
